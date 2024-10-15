4002b0a85c329a0ec2dd3105184db4b9cafffa93
Aman Choudhary <96634139+amanchoudhary08@users.noreply.github.com>
2024-10-15 05:47:04 +0000

    till now

--- JavaScript/01_basics.js
+++ JavaScript/01_basics.js
@@ -1,3 +1,58 @@
+/* NOTES
 
-const id="aman"
-console.log(id);
+const, let, var
+
+# Documnetation of JS -> tc39.es (ECMA) or MDN
+# Don't use var beacuse of issue in block scope and fucntional scope
+# "use strict" -> treat all JS code as newer version of JavaScript, but as of now, it's by default Strict
+
+# Types of DataTypes in JS
+# number, bigint, string, boolean, null(standalone value), undefined, symbol-> for unique symnbols, object
+# Null ka type object hota h.
+
+# let score = "33"
+console.log(typeof(score));
+
+let name = Number(score)
+console.log(name);
+
+# 123a ko Number() main paas krke jab iski value check krenge to NaN, and it's type is number 
+Undefined ko Number() main paas krke jab iski value check krenge to NaN, and it's type is number
+null ko Number() main paas krke jab iski value check krenge to 0, and it's type is number
+
+# "33" -> 33
+  "33a" -> NaN
+   true -> 1
+   Undefined -> NaN
+   null -> NaN
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+let score = "33"
+console.log(typeof(score));
+
+let name = Number(score)
+console.log(name);
+
+console.log(typeof(name))
+
+
+
