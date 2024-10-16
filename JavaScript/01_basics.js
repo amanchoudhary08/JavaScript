053d7144fe14e50d57787378eeae009aac951f0d
Aman Choudhary <96634139+amanchoudhary08@users.noreply.github.com>
2024-10-16 06:07:49 +0000

    completed till number and maths

--- JavaScript/01_basics.js
+++ JavaScript/01_basics.js
@@ -57,7 +57,66 @@ cl("2" === 2) -> false (strict check, also check the data type of the number)
 2. Non-Primitive: 
                 Array, Objectsn Functions
 
-#
+
+# Stack and Heap Memory
+    Stack in primitive data types,
+    e.g. let name = aman
+        let anotherName = name
+        anotherName = Aryan
+
+        but name is not changed.
+
+
+    Heap in Non-primitive data types.
+
+    let user1 = {
+        email : aman@hrs.com,
+        upi: aman@ybl
+    }
+
+    let user2 = user1
+
+    user2.email = aman@google.com
+
+    now, the email of user1 is also changed.
+
+
+#Strings in JavaScript
+
+    STRING INTERPOLATION
+        const name = "aman"
+        const age = 22;
+
+        console.log(`Hello my name is ${name} and my age is ${age}`)
+    
+    
+
+# Numbers and Maths in JS
+    const score = 100;
+    cl(score); -> 100
+
+    const ax = new Number(200);
+    cl(ax); -> [Number: 200]
+
+    cl(ax.toString()); -> 200 -> type string
+    cl(ax.toFixed(2)); -> limit the digits after decimal point to 2.
+    
+    const x = 1000000;
+    cl(x.toLocaleString()); -> 1,000,000
+
+    if we want to set the number according to the indian format, we can use 'en-IN',
+
+    cl(x.toLocaleString('en-IN')); -> 10,00,000
+    
+#  cl(Math.abs(-2)); -> 2
+    cl(Math.round(4.3)); -> 4
+    cl(Math.round(4.7)); -> 5
+    Math.ceil(-);
+    Math.floor(-);
+    Math.min(1,3,2,6); -> 1
+    Math.max(-,-,-,-); -> gives max value
+    
+    Math.random() -> gives between 0 and 1 
 
 
 
@@ -80,15 +139,43 @@ cl("2" === 2) -> false (strict check, also check the data type of the number)
 
 // console.log(id === anotherId);
 
-const heroes = ["A", "B", "C"];
-let myObj = {
-    name: "Aman",
-    age: 22,
-}
+// const heroes = ["A", "B", "C"];
+// let myObj = {
+//     name: "Aman",
+//     age: 22,
+// }
 
-const myFun = function(){
-    console.log("hello world");
+// const myFun = function(){
+//     console.log("hello world");
     
-}
+// }
 
-console.log(typeof(id));
+// console.log(typeof(id));
+
+
+// const str = new String('Aman')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase());
+
+// const str1 = str.substring(0,3)
+// console.log(str1);
+
+// const str2 = str.slice(0,3)
+// console.log(str2);
+
+// const str3 = "      amsn.1   ";
+// console.log(str3);
+
+// console.log(str3.trim());
+
+// const str4 = str3.trim();
+// console.log(str4.length);
+
+// const n = 1.223
+// console.log(n);
+// console.log(n.toFixed(0));
+
+
+// const no = new Number(200);
+// console.log(no);
