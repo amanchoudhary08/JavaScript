b8fc6f0aaa072a573d18c26337d0d87847e911c5
Aman Choudhary <96634139+amanchoudhary08@users.noreply.github.com>
2024-11-11 04:38:33 +0000

    created a new repo

--- JavaScript/04_functions.js
+++ JavaScript/04_functions.js
@@ -1,11 +1,69 @@
 /*
-        # FUNCTIONS AND PARAMETERS IN JS
+    # DIFFERENCE BETWEEN PARAMETER AND ARGUMENTS;
+    # function fn(int a, int b){          --->> a, b are PARAMETERS
+    #    console.log(a+b);
+    # }
+    #
+    # fn(3,4);                            --->> here 3,4 are ARGUMENTS
+
+    ## FUNCTIONS AND PARAMETERS IN JS
+            
+        function sayName(){
+            console.log("Aman");
+        }
+        sayName();
+
+        sayName --> function reference
+        sayName() --> function execution
         
 
+        let A = "Aman";
+        for(let i of A){
+            console.log(i);    
+        }
 
+    ## SCOPES IN JS
+
+    let a = 100;
+
+    if(true){
+        let a = 1;
+        const b= 2;
+        var c = 3;
+
+        console.log(a);            -->> 1
+    }
+        console.log(a);         -->> 100
+        console.log(b);
+        console.log(c);
+
+        a and b give error but c will print, so VAR is not good.
 
 
 
 
 
 */
+
+// function sayName(){
+//     console.log("Aman");
+// }
+
+// sayName();
+
+// "33" -> 33
+//   "33a" -> NaN
+//    true -> 1
+//    Undefined -> NaN
+//    null -> NaN
+
+// let s = null;
+// console.log(s);
+
+
+// let name = Number(s)
+// console.log(typeof name);
+
+// console.log(name);
+
+
